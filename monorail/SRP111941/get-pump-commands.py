b801fb1228bc2f6225ba41c2f0b1376d6483027c
Sofía Salazar <sogusama02@gmail.com>
2024-07-28 17:43:19 -0600

    run regulon 1

--- monorail/SRP111941/get-pump-commands.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-with open('/mnt/Citosina/amedina/ssalazar/meta/data/fastq/SRP111941/accession-list-SRP111941.txt', 'r') as fi$
-    for line in file:
-        if line !=(''):
-            lineStrip = line.rstrip('\n')
-            print('./run_pump.sh ' + lineStrip + ' ' + 'SRP111941')
